4fcdd0298ca96689504f0fe0de67a4f288a52a31
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2015-02-01 18:14:57 +0200

    Update the example

--- examples/form.php
+++ examples/form.php
@@ -1,28 +1,45 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
 $csrf = new \Riimu\Kit\CSRF\CSRFHandler();
-$csrf->validateRequest();
+
+try {
+    $csrf->validateRequest(true);
+} catch (\Riimu\Kit\CSRF\InvalidCSRFTokenException $ex) {
+    header('HTTP/1.0 400 Bad Request');
+    exit('Bad CSRF Token!');
+}
 
 $token = $csrf->getToken();
 
-if (isset($_POST['myname'])) {
-    echo "<p>Hello " . htmlspecialchars($_POST['myname']) . "!</p>";
-}
 ?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Simple Form</title>
+ </head>
+ <body>
+<?php
 
-<h1>Form with CSRF token</h1>
-<form method="post" action="form.php">
-<input type="hidden" name="csrf_token" value="<?=$token?>" />
-What is your name?
-<input type="text" name="myname" value="" />
-<input type="submit" value="Submit" />
-</form>
+if (!empty($_POST['my_name'])) {
+    printf("  <p>Hello <strong>%s!</strong></p>" . PHP_EOL, htmlspecialchars($_POST['my_name']));
+}
 
-<h1>Form without CSRF token</h1>
-<form method="post" action="form.php">
-What is your name?
-<input type="text" name="myname" value="" />
-<input type="submit" value="Submit" />
-</form>
+?>
+  <h3>Form with a CSRF token:</h3>
+  <form method="POST" action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>"><div>
+   <input type="hidden" name="csrf_token" value="<?=htmlspecialchars($token)?>" />
+   What is your name?
+   <input type="text" name="my_name" />
+   <input type="submit" />
+  </div></form>
+
+  <h3>Form without a CSRF token:</h3>
+  <form method="POST" action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>"><div>
+   What is your name?
+   <input type="text" name="my_name" />
+   <input type="submit" />
+  </div></form>
+ </body>
+</html>
